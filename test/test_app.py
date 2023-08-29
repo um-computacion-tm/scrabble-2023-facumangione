93db50f8964574829fefbfb9bf1b0e86eeed31e9
facundo <f.mangione@alumno.um.edu.ar>
2023-08-29 16:33:41 -0300

    cambio_De_ambiente

---
+++ test/test_app.py
@@ -0,0 +1,8 @@
+import unittest 
+class TestMulti(unittest.TestCase):
+    def test_mul(self):
+        self.assertEqual(multiplicacion(2,3),6)
+        
+if __name__ == "__main__":
+    unittest.main
+       

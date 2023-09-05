6c547755531e9b8251fb06343333feca08eac659
facundo <f.mangione@alumno.um.edu.ar>
2023-09-05 17:56:30 -0300

    orden_archivos

--- test/test_app.py
+++
@@ -1,9 +0,0 @@
-import unittest 
-class TestMulti(unittest.TestCase):
-    def test_mul(self):
-        self.assertEqual(multiplicacion(2,3),6)
-        
-if __name__ == "__main__":
-    unittest.main
-       
-from multi import multiplicacion       

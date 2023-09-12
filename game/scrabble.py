b02a635fb1acff5d7154bc2e569e3007839fbcbf
facundo <f.mangione@alumno.um.edu.ar>
2023-09-12 19:20:50 -0300

    player

--- game/scrabble.py
+++ game/scrabble.py
@@ -12,7 +12,6 @@ class ScrabbleGame:
         for _ in range(players_count):
              self.players.append(Player(id=len(self.players) + 1))# Quitamos el argumento 'id'
         self.current_player = None
-
     def next_turn(self):
         if self.current_player is None:
             self.current_player = self.players[0]
@@ -20,12 +19,4 @@ class ScrabbleGame:
             self.current_player = self.players[0]
         else:
             self.current_player = self.players[self.players.index(self.current_player)+ 1]
-
-    def test_next_turn_when_player_is_last():
-    # Suponiendo que tenemos 3 jugadores, luego del jugador 3, le toca al jugador 1
-        scrabble_game = ScrabbleGame(players_count=3)
-        scrabble_game.current_player = scrabble_game.players[2]
-
-        scrabble_game.next_turn()
-
-        assert scrabble_game.current_player == scrabble_game.players[0]
+            

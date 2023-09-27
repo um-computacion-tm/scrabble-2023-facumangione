ffda4a14e88c5e576f5bbc7f7c68659e830f1d06
facundo <f.mangione@alumno.um.edu.ar>
2023-09-27 09:22:14 -0300

    prueba

--- test_proyecto/test_player.py
+++ test_proyecto/test_player.py
@@ -3,6 +3,7 @@ from game.game_player import Player
 from game.game_cell import Cell
 from game.models import Tile
 from game.game_board import Board
+from game.models import BagTiles
 
 
 class TestPlayer(unittest.TestCase):
@@ -37,6 +38,51 @@ class TestPlayer(unittest.TestCase):
 
         # Caso de prueba inválido
         self.assertFalse(self.player.validate_word('ABE'))
+
+def test_validate_user_has_letters(self):
+        bag_tile = BagTiles()
+        bag_tile.tiles = [
+            Tile(letter='H', value=1),
+            Tile(letter='O', value=1),
+            Tile(letter='L', value=1),
+            Tile(letter='A', value=1),
+            Tile(letter='C', value=1),
+            Tile(letter='U', value=1),
+            Tile(letter='M', value=1),
+        ]
+        player = Player(bag_tile)
+        tiles = [
+            Tile(letter='H', value=1),
+            Tile(letter='O', value=1),
+            Tile(letter='L', value=1),
+            Tile(letter='A', value=1),
+        ]
+
+        is_valid = player.has_letters(tiles)
+
+        self.assertEqual(is_valid, True)
+
+        def test_validate_fail_when_user_has_not_letters(self):
+            bag_tile = BagTiles()
+        bag_tile.tiles = [
+            Tile(letter='P', value=1),
+            Tile(letter='O', value=1),
+            Tile(letter='L', value=1),
+            Tile(letter='A', value=1),
+            Tile(letter='C', value=1),
+            Tile(letter='U', value=1),
+            Tile(letter='M', value=1),
+        ]
+        player = Player(bag_tile)
+        tiles = [
+            Tile(letter='H', value=1),
+            Tile(letter='O', value=1),
+            Tile(letter='L', value=1),
+            Tile(letter='A', value=1),
+        ]
+
+        is_valid = player.has_letters(tiles)
+        self.assertEqual(is_valid, False)
 
 if __name__ == '__main__':
     unittest.main()
